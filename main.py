a7972b2127a74b5f1747d7cf18bf1af289168587
grey sky <grey@debian.grey>
2020-04-28 21:31:46 +0300

    main file added

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import gauss_jordan as gj
 
-matrix1 = gj.Matrix([[1, 2, 3], [4, 5, 6], [0, 0, 1]])
+matrix1 = gj.Matrix([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
 matrix2 = gj.reduced_echelon(matrix1)
 matrix2.print()
